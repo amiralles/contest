8eae789128b48551f7b5ed633f413511bfe5c8c9
amiralles <info@amiralles.com.ar>
2015-04-15 18:20:26 -0300

    merge contest assms

--- src/Contest.Core/TestSuite.cs
+++ src/Contest.Core/TestSuite.cs
@@ -1,7 +1,10 @@
 namespace Contest.Core {
     using System.Collections.Generic;
 
-    public class TestSuite{
+    public class TestSuite {
         public readonly List<TestCase> Cases = new List<TestCase>();
+	
+		public TestSuite() {
+		}
     }
 }
