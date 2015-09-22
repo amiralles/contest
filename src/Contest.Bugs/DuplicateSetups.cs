305363587df444c7de1417c0ed6606d19bc88772
amiralles <amiralles.net@gmail.com>
2015-09-22 17:51:08 -0300

    refactor 'setupmess' test cases

--- src/Contest.Bugs/DuplicateSetups.cs
+++ src/Contest.Bugs/DuplicateSetups.cs
@@ -1,17 +1,21 @@
 using _ = System.Action<Contest.Core.Runner>;
 using static System.Console;
 
-class fix_one {
-	_ before_each = test => {
-		WriteLine(">>>> setup");
-	};
+class fixure_wide_setup_teardown {
+	_ before_each = test => { WriteLine(">>>> fixture setup"); };
+	_ after_each  = test => { WriteLine(">>>> fixture teardown"); };
+	 
 
 	_ foo = assert => assert.Pass();
 	_ bar = assert => assert.Pass();
 }
 
-class fix_two {
+class case_specific_setup_teardown {
 
+	_ before_foo = test => { WriteLine(">>>> foo setup"); };
+	_ after_foo  = test => { WriteLine(">>>> foo teardown"); };
+
+	_ foo      = assert => assert.Pass();
 	_ baz	   = assert => assert.Pass();
 	_ bazzinga = assert => assert.Pass();
 }
